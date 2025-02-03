53b270790d1c67721e516f4ae53b909d46c921e6
Cy83r80ii <Cy83r80ii@proton.me>
2025-02-03 22:45:56 +0530

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./footer.css";
 import { ReactComponent as Copyright } from "../../assets/copyright.svg";
-import { Margin } from "@mui/icons-material";
 const footer = () => {
   const developedBy = () => {
-       window.open("https://github.com/shadowctrl/Hackatank", "_self");
-    };
+    window.open("https://shadowctrl.me", "_self");
+    window.open("https://upwork.com/freelancers/shadowctrl", "_blank");
+  };
   return (
     <React.Fragment>
       <div className="parent-footer">
         <p className="copyright">
           Copyrights
           <Copyright className="copyright-icon" />
-          2025-2026 - {"All Rights Reserved."}
+          2024-2025 - {"All Rights Reserved."}
         </p>
         <p className="copyright">
-          Developed & Maintained by 
-          <p onClick={BATMAN}> shadowctrlz</p><p>,</p><div style= {{marginLeft: "6px"}}>
-     
-          
-          
+          Developed & Maintained by <p onClick={developedBy}>shadowctrl</p>
         </p>
       </div>
     </React.Fragment>
